ef8387d6e7177f916fe7db886e6c8d6169c9c016
hiram64 <carlosinqta@gmail.com>
2019-10-26 22:35:02 +0900

    revise download script

--- make_cifar10_npz.py
+++ make_cifar10_npz.py
@@ -1,4 +1,5 @@
 import glob
+import os
 import pickle
 
 import cv2
@@ -8,27 +9,29 @@ import numpy as np
 # You can download cifar10 data via https://www.kaggle.com/janzenliu/cifar-10-batches-py
 data_dir = './data'
 
-all_files = glob.glob(data_dir + '/data_batch' + '*')
-test_files = glob.glob(data_dir + '/test_batch' + '*')
+all_files = glob.glob(os.path.join(data_dir, 'data_batch*'))
+test_files = glob.glob(os.path.join(data_dir, 'test_batch*'))
 
 all_files = all_files + test_files
+assert all_files, 'File paths to load are empty. Please ensure downloaded files are in {}'.format(data_dir)
 
 
 def unpickle(file):
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        dic = pickle.load(fo, encoding='bytes')
+    return dic
 
 
-# labal
+# label
 # 0:airplane, 1:automobile, 2:bird. 3:cat, 4:deer, 5:dog, 6:frog, 7:horse, 8:ship, 9:truck
 all_image = []
 all_label = []
 
+print('Load compressed files')
 print(all_files)
 
 for file in all_files:
-    print(file)
+    print('Processing', file)
     ret = unpickle(file)
 
     for i, arr in enumerate(ret[b'data']):
@@ -40,6 +43,11 @@ for file in all_files:
 
 all_images = np.array(all_image)
 all_labels = np.array(all_label)
+
+print('All images array shape')
 print(all_images.shape)
 
-np.savez(data_dir + '/' + 'cifar10.npz', images=all_images, labels=all_labels)
+print('All labeles array shape')
+print(all_labels.shape)
+
+np.savez(os.path.join(data_dir, 'cifar10.npz'), images=all_images, labels=all_labels)
